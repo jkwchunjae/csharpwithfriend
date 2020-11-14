a573b8fc746c73cdc937143583d30b0eadbdb48d
jkw <jkwchunjae@daum.net>
2020-11-14 22:08:18 +0900

    일기 쓰기

--- DiaryForm/DiaryForm/Form1.cs
+++ DiaryForm/DiaryForm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,19 @@ namespace DiaryForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var date = dateTimePicker1.Value;
+            var text = richTextBox1.Text;
+
+            var path = @"C:\Users\jkwch\Documents\diary\test";
+            // var fileName0 = date.ToString("yyyyMMdd") + ".txt"; // 20201114.txt
+            var fileName = $"{date:yyyyMMdd}.txt"; // 20201114.txt
+            var diaryPath = Path.Combine(path, fileName);
+
+            File.WriteAllText(diaryPath, text);
         }
     }
 }
