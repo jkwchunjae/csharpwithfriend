d1e15a86ecfc17ac9d6f3d7c2a27be7fb695f159
jkw <jkwchunjae@daum.net>
2020-12-05 22:24:13 +0900

    TagControlData 파일로 분리

--- DiaryForm/DiaryForm/Form1.cs
+++ DiaryForm/DiaryForm/Form1.cs
@@ -12,14 +12,6 @@ using System.Windows.Forms;
 
 namespace DiaryForm
 {
-    public class TagControlData
-    {
-        public int Index { get; set; }
-        public string TagText { get; set; }
-        public Label Control { get; set; }
-        public Button DeleteButton { get; set; }
-    }
-
     public partial class Form1 : Form
     {
         private List<TagControlData> _tagData = new List<TagControlData>();
---
+++ DiaryForm/DiaryForm/TagControlData.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace DiaryForm
+{
+    public class TagControlData
+    {
+        public int Index { get; set; }
+        public string TagText { get; set; }
+        public Label Control { get; set; }
+        public Button DeleteButton { get; set; }
+    }
+}
